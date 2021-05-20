3d54aaca5818bff896b69e6c4eab400ed3389549
seoyeonko <seoyeon99o@naver.com>
2021-05-20 23:35:43 +0900

    컴포넌트 UI 완성

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -3,30 +3,74 @@ import styled, { css } from "styled-components";
 import { MdAdd } from "react-icons/md";
 
 const CircleButton = styled.button`
-  width: 64px;
-  height: 64px;
-  border-radius: 32px;
-  background-color: #74b9ff;
-  border: none;
-  color: #ffffff;
-
+  background: #38d9a9;
   &:hover {
-    // mouse over
-    background-color: #0984e3;
+    background: #63e6be;
   }
   &:active {
-    // mouse click
-    background-color: #ff7675;
+    background: #20c997;
   }
 
-  font-weight: bold;
-  font-size: 32px;
+  z-index: 5;
+  cursor: pointer;
+  width: 80px;
+  height: 80px;
+  display: block;
   align-items: center;
-  text-align: center;
+  justify-content: center;
+  font-size: 60px;
+  position: absolute;
+  left: 50%;
+  bottom: 0px;
+  transform: translate(-50%, 50%);
+  color: white;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: 0.125s all ease-in;
+  ${(props) =>
+    props.open &&
+    css`
+      background: #ff6b6b;
+      &:hover {
+        background: #ff8787;
+      }
+      &:active {
+        background: #fa5252;
+      }
+      transform: translate(-50%, 50%) rotate(45deg);
+    `}
 `;
-const InsertFromPositioner = styled.div``;
-const InsertForm = styled.form``;
-const Input = styled.input``;
+const InsertFromPositioner = styled.div`
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+`;
+const InsertForm = styled.form`
+  background: #ffe6e6;
+  padding-left: 32px;
+  padding-top: 32px;
+  padding-right: 32px;
+  padding-bottom: 72px;
+
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  /* border-top: 1px solid #e9ecef; */
+`;
+const Input = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #778ca3;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
+`;
 
 function TodoCreate() {
   const [open, setOpen] = useState(false);
@@ -34,6 +78,13 @@ function TodoCreate() {
 
   return (
     <>
+      {open && (
+        <InsertFromPositioner>
+          <InsertForm>
+            <Input autoFocus placeholder="할 일 입력 후, Enter 누르세요" />
+          </InsertForm>
+        </InsertFromPositioner>
+      )}
       <CircleButton onClick={onToggle} open={open}>
         <MdAdd />
       </CircleButton>
